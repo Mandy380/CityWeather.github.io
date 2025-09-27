9ef26a419947c94b431307a95b72d27a357b1731
Mandy380 <72581127+Mandy380@users.noreply.github.com>
2025-09-28 00:52:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getCityWeather() {
-  const city = document.getElementById('cityInput').value;
+  const city = document.getElementById('name').value;
   const apiKey = '23311bd3097a594bc9e6c697cbd6df54'; 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
@@ -14,12 +14,12 @@ async function getCityWeather() {
         <p>☁️ Weather: ${data.weather[0].description}</p>
         <p>💨 Wind: ${data.wind.speed} m/s</p>
       `;
-      document.getElementById('wResult').innerHTML = result;
+      document.getElementById('Results').innerHTML = result;
     } else {
-      document.getElementById('Result').innerHTML = `<p>City not found.</p>`;
+      document.getElementById('Results').innerHTML = `<p>City not found.</p>`;
     }
   } catch (error) {
-    document.getElementById('Result').innerHTML = `<p>Error fetching weather data.</p>`;
+    document.getElementById('Results').innerHTML = `<p>Error fetching weather data.</p>`;
   }
 }
 
